95da72fb7ebd710f2c5735d29b0a8094f5e50ec9
Ayush Rawat <ayushrawat75847@gmail.com>
2026-01-08 15:48:49 +0530

    added navbar to all pages with some styling og it

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname,"public")));
 
 const MONGOOSE_URL="mongodb://127.0.0.1:27017/nestio";
 
