b78084bcc5d2e43ee74b2f5d3273e4593f10bd45
Ayush Rawat <ayushrawat75847@gmail.com>
2026-01-09 14:14:36 +0530

    added styling to all pages

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/listings/:id/edit", async (req,res)=>{
 //Update Route
 app.put("/listings/:id", async (req,res)=>{
      let {id}= req.params;
-     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+     await Listing.findByIdAndUpdate(id, {...req.body.listing},{runValidators: true, runSettersOnQuery: true});
     res.redirect(`/listings/${id}`);
 });
 
--- models/listing.js
+++ models/listing.js
@@ -15,8 +15,13 @@ const listingSchema = new Schema({
         },
         url: {
             type: String,
-            default: "https://www.shutterstock.com/search/scenery",
-            set: v => v === "" ? "https://www.shutterstock.com/search/scenery" : v
+            default: "https://imgs.search.brave.com/x6uHDei63r2U35HbRncFplw4LXWp_kJI2hhbSbglp9A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMjA2/MzU5L3BleGVscy1w/aG90by0yMDYzNTku/anBlZz9hdXRvPWNv/bXByZXNzJmNzPXRp/bnlzcmdiJmRwcj0x/Jnc9NTAw",
+            set: v => {
+                if (!v || v.trim() === "") {
+                    return "https://imgs.search.brave.com/x6uHDei63r2U35HbRncFplw4LXWp_kJI2hhbSbglp9A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMucGV4ZWxzLmNv/bS9waG90b3MvMjA2/MzU5L3BleGVscy1w/aG90by0yMDYzNTku/anBlZz9hdXRvPWNv/bXByZXNzJmNzPXRp/bnlzcmdiJmRwcj0x/Jnc9NTAw";
+                }
+                return v;
+            }
         }
     },
     price: Number,
