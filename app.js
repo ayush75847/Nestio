9529d2cfd301e37e0aa57352157a415b136d9812
Ayush Rawat <ayushrawat75847@gmail.com>
2026-01-13 16:33:40 +0530

    Add centralized error-handling middleware and custom Error class

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing= require("./models/listing.js");
 const path= require("path");
 const methodOverride= require("method-override");
 const ejsMate= require("ejs-mate");
+const wrapAsync= require("./utils/wrapAsync.js");
+const ExpressError= require("./utils/ExpressError.js");
+const {listingSchema}= require("./schema.js");
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
@@ -28,17 +31,27 @@ async function main() {
     
 };
 
+const listingValidate= (req,res,next)=>{
+    let{error}= listingSchema.validate(req.body);
+    if(error){
+        const errMsg= error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
 //Home Route
 app.get("/",(req, res)=>{
     res.send("Hello, this is the root node");
 });
 
 //Index Route
-app.get("/listings", async (req,res)=>{
+app.get("/listings", wrapAsync( async (req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{ allListings });
 
-})
+}));
 
 //New Route
 app.get("/listings/new",(req,res)=>{
@@ -46,39 +59,50 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //Create Route
-app.post("/listings/create",async (req,res)=>{
+app.post("/listings/create",listingValidate, wrapAsync(async (req,res,next)=>{
     let newListing= new Listing(req.body.listing);
-    newListing.save();
+    await newListing.save();
     res.redirect("/listings");
-})
+}));
 
 //Edit route
-app.get("/listings/:id/edit", async (req,res)=>{
+app.get("/listings/:id/edit", wrapAsync( async (req,res)=>{
     let {id}=req.params;
     const listing= await Listing.findById(id);
     res.render("listings/edit.ejs",{ listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req,res)=>{
+app.put("/listings/:id",listingValidate, wrapAsync( async (req,res)=>{
      let {id}= req.params;
      await Listing.findByIdAndUpdate(id, {...req.body.listing},{runValidators: true, runSettersOnQuery: true});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id", wrapAsync( async (req,res)=>{
     let {id}= req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
-})
+}));
 
 //Show Route
-app.get("/listings/:id", async (req,res)=>{
+app.get("/listings/:id", wrapAsync( async (req,res)=>{
 
     let {id}= req.params;
     const listing= await Listing.findById(id);
     res.render("listings/show.ejs",{ listing });
+}));
+
+
+// Middleware for non existing pages Error Handling 
+app.use( (req,res,next)=>{
+    next(new ExpressError(404,"Page not Found!"));
+});
+
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="Something went incorrect"}= err;
+    res.status(statusCode).render("listings/error.ejs",{message});
 });
 
 app.listen(8080, ()=>{
---
+++ schema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+module.exports.listingSchema= Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        image: Joi.object({
+            filename: Joi.string().optional(),
+            url: Joi.string().allow("").optional(),
+        }).optional(),
+    }).required()
+});
+
